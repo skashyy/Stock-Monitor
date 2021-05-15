4cc0186d0ea0d20ca5b565f1bd98ad0297374ef0
Sasha Wallace <aow6@case.edu>
2021-05-15 17:06:48 -0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Stock-Monitor
+Require pygame to playsounds, and request to handle http(s) requests. 
+
+Instructions for installing pygame:
+Windows:
+Type the following into command line:
+py -m pip install -U pygame
+Mac OS:
+python3 -m pip install -U pygame
+Debian/Ubuntu/Mint
+sudo apt-get install python3-pygame
+
+Instructions for installing request:
+python -m pip install requests
--- scraper.py
+++ scraper.py
@@ -1,51 +1,84 @@
-from urllib.request import urlopen
+import requests
 from pygame import mixer
 import time
- 
+import random
 
-def run():
-    mixer.init()
-    mixer.music.load("sound.mp3")
-    while True:
-        neweggfile = open('newegg.txt') 
-        for line in neweggfile:
-            p1 = line[:line.index(" ")]
-            p2 = line[line.index(" "):]
-            url = "https://www.newegg.com/p/" + p1
-            
-            page = urlopen(url)
-            html_bytes = page.read()
-            html = html_bytes.decode("utf-8")
-            print(html)
-            if not "OUT OF STOCK" in html and "FinalPrice" in html:
-                priceind = html.index("FinalPrice")
-                poop = html[priceind+12:priceind+20]
-                pricecomma = 3 + poop[3:].index(",")
-                price = poop[:pricecomma]
-                if price < p2:
-                    nameind = 15 + html.index("ProductTitle")
-                    namecomma = nameind + html[nameind:].index('"')
-                    name= html[nameind:namecomma]
-                    print("")
-                    print(name + " ")
-                    print(price)
-                    print(url)
-                    mixer.music.play()
-                    print("hello rice im right here!!!!")
-                    
-            time.sleep(100)
-                
-                
-                
+mixer.init()
+mixer.music.load("sound.mp3")
 
-        neweggfile.close()
+proxies = []
+proxyfile = open('proxies.txt')
+user = "sashaonln1@gmail.com"
+password = "r3R!W3w2e+2APf"
+port = "8080"
+for line in proxyfile:
+    proxies.append(user + ":" + password + "@" + line.strip("\n") + ":" + port)
+proxyfile.close()
+
+useragents = []
+useragentfile = open('useragents.txt')
+for line in useragentfile:
+    useragents.append(line.strip("\n"))
+useragentfile.close()
+
+def runNewegg():
+    neweggfile = open('newegg.txt') 
+    for line in neweggfile:
+        p1 = line[:line.index(" ")]
+        p2 = line.replace("\n", "")[line.index(" ") + 1:]
+        url = "https://www.newegg.com/p/" + p1
+        session=requests.session()
+        prox = random.choice(proxies)
+        proxy = {"http": prox, "https": prox}
+        header = {'User-Agent' : random.choice(useragents)}
+        html = session.get(url, headers = header, proxies = proxy).text
+        if (not "OUT OF STOCK" in html) and "FinalPrice" in html:
+            priceind = html.index("FinalPrice")
+            pricesubstring = html[priceind+12:priceind+20]
+            pricecomma = 3 + pricesubstring[3:].index(",")
+            price = pricesubstring[:pricecomma]
+            if float(price) < float(p2):
+                nameind = 15 + html.index("ProductTitle")
+                namecomma = nameind + html[nameind:].index('"')
+                name= html[nameind:namecomma]
+                print("\n" + name + "\n$"+ price + "\n" + url)
+                mixer.music.play()        
+        time.sleep(.1)
+    neweggfile.close()
         
             
             
-            
+def runBestBuy():
+    bestbuyfile = open('bestbuy.txt')
+    for line in bestbuyfile:
+        url = "https://www.bestbuy.com/site/" + line.strip("\n") + ".p?skuId=" + line.strip("\n")
+        session = requests.session()
+        prox = random.choice(proxies)
+        proxy = {"http": prox, "https": prox}
+        header = {'User-Agent':random.choice(useragents)}
+        html = session.get(url, headers = header).text
+        print("hello")
+        if "ADD_TO_CART" in html[html.find("buttonState") + 16 : html.find("buttonState") + 30]:
+            priceind = html.index(',\"price\":')
+            pricesubstring = html[priceind+10:priceind+20]
+            pricecomma = 2 + pricesubstring[2:].index(",")
+            price = pricesubstring[:pricecomma - 1]
+            nameind = 8 + html.index("<title >")
+            name = html[nameind:html.index(" - Best Buy</title>")]
+            print("\n" + name + "\n$"+ price + "\n" + url)
+            mixer.music.play()  
+        time.sleep(.1)
+        
+    bestbuyfile.close()
+
+
             
     
     
 
 
+def run():
+    while True:
+        runNewegg()
+        runBestBuy()
 run()
